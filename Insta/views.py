cff9a243373ecb0d0159c9de7bfb00322884f6a2
hungshe <hungshe@umich.edu>
2019-12-31 22:21:29 -0800

    Auth/Login/Logout/SignUp/Customized Signup Form Practice

--- Insta/views.py
+++ Insta/views.py
@@ -3,6 +3,9 @@ from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
 from Insta.models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
+#from django.contrib.auth.forms import UserCreationForm
+from Insta.forms import CustomUserCreationForm
 
 class HelloWorld(TemplateView):
         template_name  =  'test.html'
@@ -17,19 +20,27 @@ class PostDetailView(DetailView):
         model  =  Post
         template_name  =  'post_detail.html'
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
         model  =  Post
         template_name  =  'post_create.html'
         fields  =  '__all__' #allow user to provide all fields
+        login_url  =  'login' 
 
-class PostUpdateView(UpdateView):
+class PostUpdateView(LoginRequiredMixin, UpdateView):
         model  =  Post
         template_name  =  'post_update.html'
         fields  =  ['title'] #allow user to update title only
+        login_url  =  'login' 
 
-class PostDeleteView(DeleteView):
+class PostDeleteView(LoginRequiredMixin, DeleteView):
         model  =  Post
         template_name  =  'post_delete.html'
         success_url  =  reverse_lazy('helloworld') 
         # Invalid operation if we call reverse only, 
         #  it's simutaneously delete and redirect to success url, use reverse_lazy instead
+        login_url  =  'login' 
+
+class Signup(CreateView):
+        form_class  =  CustomUserCreationForm #UserCreationForm #UserCreationForm is limited, need to create user customized form to replace
+        template_name  =  'signup.html'
+        success_url  =  reverse_lazy("login")
