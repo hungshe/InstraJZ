da002744ffd013b46b603d17d07e06b8bd0e8089
hungshe <hungshe@umich.edu>
2020-01-03 13:01:44 -0800

    Extending templates/CSS/Bootstrap/Font-Awesome Add Comment/Like/Profile System/Ajax

--- Insta/models.py
+++ Insta/models.py
@@ -3,18 +3,6 @@ from django.contrib.auth.models import AbstractUser
 from django.urls import reverse
 from imagekit.models import ProcessedImageField
 # Create your models here.
-class Post(models.Model):
-        title  =  models.TextField(blank=True, null=True)
-        image  =  ProcessedImageField(
-                upload_to='static/images/posts',
-                format='JPEG',
-                options={'quality':100},
-                blank=True,
-                null=True
-                )
-                
-        def get_absolute_url(self):
-                return  reverse("post_detail", args=[str(self.id)])
 # Define User Customized  User model
 class InstaUser(AbstractUser):
         profile_pic  =  ProcessedImageField(
@@ -24,3 +12,55 @@ class InstaUser(AbstractUser):
                 blank=True,
                 null=True
                 )
+
+class UserConnection(models.Model):
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    creator = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name="friendship_creator_set")
+    following = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name="friend_set")
+
+    def __str__(self):
+        return self.creator.username + ' follows ' + self.following.username
+
+class Post(models.Model):
+        author  =  models.ForeignKey(
+               InstaUser,
+               on_delete = models.CASCADE,
+               related_name='my_posts' 
+        )
+        title  =  models.TextField(blank=True, null=True)
+        image  =  ProcessedImageField(
+                upload_to='static/images/posts',
+                format='JPEG',
+                options={'quality':100},
+                blank=True,
+                null=True
+                )
+
+        def get_like_count(self): 
+                return self.likes.count()
+                      
+        def get_absolute_url(self):
+                return  reverse("post_detail", args=[str(self.id)])
+
+#  user name like post x
+# when post x is deleted, like also removed
+class Like(models.Model):
+        post   =  models.ForeignKey(
+                Post,
+                on_delete=models.CASCADE,
+                related_name='likes')
+        user  = models.ForeignKey(
+                InstaUser,
+                on_delete=models.CASCADE,
+                related_name='likes')
+        class Meta:
+                unique_together  =  ("post", "user")
+        
+        def __str__(self):
+                        return 'Like: ' + self.user.username  +  '  likes  '  + self.post.title
--- Insta/views.py
+++ Insta/views.py
@@ -1,8 +1,8 @@
 from django.views.generic import TemplateView, ListView, DetailView
-
+from annoying.decorators import ajax_request
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
-from Insta.models import Post
+from Insta.models import Post, Like, InstaUser, UserConnection
 from django.contrib.auth.mixins import LoginRequiredMixin
 #from django.contrib.auth.forms import UserCreationForm
 from Insta.forms import CustomUserCreationForm
@@ -15,10 +15,22 @@ class PostsView(ListView):
         model  =  Post
         template_name  =  'index.html'
 
+        def get_queryset(self):
+                current_user = self.request.user
+                following = set()
+                for conn in UserConnection.objects.filter(creator=current_user).select_related('following'):
+                        following.add(conn.following)
+                return Post.objects.filter(author__in=following)
+
 # define  detailed view of posts
 class PostDetailView(DetailView):
         model  =  Post
         template_name  =  'post_detail.html'
+
+# define  detailed view of posts
+class UserDetailView(DetailView):
+        model  =  InstaUser
+        template_name  =  'user_detail.html'
 
 class PostCreateView(LoginRequiredMixin, CreateView):
         model  =  Post
@@ -44,3 +56,21 @@ class Signup(CreateView):
         form_class  =  CustomUserCreationForm #UserCreationForm #UserCreationForm is limited, need to create user customized form to replace
         template_name  =  'signup.html'
         success_url  =  reverse_lazy("login")
+# @ means only reflect to ajax_request, no need to be any template
+@ajax_request
+def addLike(request):
+    post_pk = request.POST.get('post_pk')
+    post = Post.objects.get(pk=post_pk)
+    try:
+        like = Like(post=post, user=request.user)
+        like.save()
+        result = 1
+    except Exception as e:
+        like = Like.objects.get(post=post, user=request.user)
+        like.delete()
+        result = 0
+
+    return {
+        'result': result,
+        'post_pk': post_pk
+    }
