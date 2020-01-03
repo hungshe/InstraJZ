4d5eabcbc5d697d23f755439cca9ceabd90a0327
hungshe <hungshe@umich.edu>
2020-01-03 13:56:23 -0800

    Prep for deployment- Missing whitenoisestorage

--- InstaJZ/settings.py
+++ InstaJZ/settings.py
@@ -125,6 +125,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS  = [os.path.join(BASE_DIR,  'static')]
 
+STATICFILES_STORAGE  =  'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_REDIRECT_URL  =  'posts'
 
 LOGOUT_REDIRECT_URL  =  'posts'
